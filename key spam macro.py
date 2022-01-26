761ccace87935dc4c2a649ba7f785e8689493b8d
RareDrops <54132759+RareDrops@users.noreply.github.com>
2022-01-27 01:58:05 +1300

    Add files via upload

---
+++ key spam macro.py
@@ -0,0 +1,17 @@
+from flask import Config
+import win32api, win32con
+import keyboard
+from configparser import ConfigParser
+config = ConfigParser
+config.read("config.ini")
+
+
+
+def leftClick():
+    for _ in range(10):
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+
+keyboard.add_hotkey(f"{config.get('main','hotkey_to_press')}",leftClick)
+keyboard.wait()
